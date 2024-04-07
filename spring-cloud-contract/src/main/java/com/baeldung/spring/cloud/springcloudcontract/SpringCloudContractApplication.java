9e4a50ffb5a368cc303e4ec3683fef322f7b4370
J. Junior <junior.jaava@gmail.com>
2024-04-07 21:52:11 +0200

    first full commit

---
+++ spring-cloud-contract/src/main/java/com/baeldung/spring/cloud/springcloudcontract/SpringCloudContractApplication.java
@@ -0,0 +1,13 @@
+package com.baeldung.spring.cloud.springcloudcontract;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudContractApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudContractApplication.class, args);
+	}
+
+}
